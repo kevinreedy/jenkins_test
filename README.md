4fa960e045267915b78d04be716d1bfb2ccb6f26
Kevin Reedy <kevinreedy@gmail.com>
2018-01-11 10:47:36 -0800

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# jenkins_test
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: jenkins_test
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
